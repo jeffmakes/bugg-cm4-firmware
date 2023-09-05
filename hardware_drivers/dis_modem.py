3580b05b3a70a55ae0596fc6ebcabed3e6dc9ee1
Jeff Gough <j@monad.is>
2023-09-05 13:05:10 +0100

    Script for turning off the modem

---
+++ hardware_drivers/dis_modem.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time 
+
+en_pin=7
+power_on_n_pin = 5
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(en_pin, GPIO.OUT)
+GPIO.setup(power_on_n_pin, GPIO.OUT)
+
+GPIO.output(power_on_n_pin,1)
+time.sleep(1)
+GPIO.output(power_on_n_pin,0)
+time.sleep(10)
+GPIO.output(en_pin,0)
+time.sleep(1)
